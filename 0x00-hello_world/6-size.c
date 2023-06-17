76534a41339417fb3e40b9f263b27e856ee9694b
JuggernautTej <tiwatej@gmail.com>
2023-06-17 08:34:19 -0700

    another update for the size program2

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -15,7 +15,7 @@ long long int b;
 printf("Size of a char: %d byte(s)\n", (int) sizeof(c));
 printf("Size of an int: %d byte(s)\n", (int) sizeof(i));
 printf("Size of a long int: %lu byte(s)\n", sizeof(k));
- printf("Size of a long long int: %llu byte(s)\n", (long long int)sizeof(b));
+printf("Size of a long long int: %llu byte(s)\n", (long long int)sizeof(b));
 printf("Size of a float: %lu byte(s)\n", sizeof(z));
 return (0);
 }
