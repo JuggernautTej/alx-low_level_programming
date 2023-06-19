b202e5f7a343f56d04d6edf5a006267ed5b74817
JuggernautTej <tiwatej@gmail.com>
2023-06-19 11:22:45 -0700

    added the 3-print_alphabets program

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+/**
+ *main - Entry point
+ *
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+char beta;
+
+for (beta = 'a'; beta <= 'z' ; beta++)
+{
+putchar(beta);
+}
+ for (beta= 'A'; beta <= 'Z'; beta++)
+   {putchar (beta);}
+ putchar('\n');
+return (0);
+}
