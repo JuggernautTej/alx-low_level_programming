041dd0c32b7324b2307cfccc0917bac5b70158a0
JuggernautTej <tiwatej@gmail.com>
2023-06-25 16:24:31 -0700

    added the 1-alphabet program

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ *main - Entry point
+ *Return: Always 0
+ */
+void print_alphabet(void);
+{
+char beta;
+for(beta = 'a'; beta <= 'z'; beta++)
+{
+_putchar(beta);
+}
+_putchar('\n');
+return (0);
+}
