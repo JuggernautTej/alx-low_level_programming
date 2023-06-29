3c181c2a7a76e89c4f4cb894aeec14828a293c13
JuggernautTej <tiwatej@gmail.com>
2023-06-29 05:54:50 -0700

    updated puts2 program3

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -9,13 +9,12 @@
  */
 void puts2(char *str)
 {
-if (str == '\0')
+if (*str == '\0')
 {
 return;
 }
-int i = 0;
 
-for (i = 0; str[i] != '\0'; i += 2)
+for (int i = 0; str[i] != '\0'; i += 2)
 {
 _putchar(str[i]);
 }
