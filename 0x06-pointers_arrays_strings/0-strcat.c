1eaf41cb6f3acee9fecc09930616ed1c42a677f0
JuggernautTej <tiwatej@gmail.com>
2023-06-29 09:02:02 -0700

    updated the strcat program4

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -10,7 +10,15 @@
  */
 char *_strcat(char *dest, char *src)
 {
-_strcat(dest,src);
+while (*dest != '\0')
+{
+dest++;
+}
+while (*src != '\0')
+{
+*dest = *src;
+dest++;
+src++;
+}
 *dest = '\0';
-return (dest);
 }
