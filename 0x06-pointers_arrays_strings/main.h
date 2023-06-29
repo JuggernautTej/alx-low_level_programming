27927dc03d273d92c5e0c301c00e030c000d304d
JuggernautTej <tiwatej@gmail.com>
2023-06-29 12:52:27 -0700

    updated the strcat program 10

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,14 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-int _putchar(char);
-void print_alphabet(void);
-void reset_to_98(int *n);
-void swap_int(int *a, int *b);
-int _strlen(char *s);
-void print_rev(char *s);
-void rev_string(char *s);
-void puts2(char *str);
 char *_strcat(char *dest, char *src);
 
 #endif
