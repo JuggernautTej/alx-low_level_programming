05cff060e2473dd4ac122411689678bb87a0398a
JuggernautTej <tiwatej@gmail.com>
2023-06-29 07:40:42 -0700

    added the strcat program, readme and new directory

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ *_strcat - A function that concatenates strings.
+ *
+ *@dest; This is a pointer variable to a character
+ *@src; Same as dest.
+ *
+ *Return: Always 0.
+ */
+char *_strcat(char *dest, char *src)
+{
+  _strcat(dest,src);
+  return (dest);
+}
---
+++ 0x06-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+This is another directory tackling pointers, arrays and strings
