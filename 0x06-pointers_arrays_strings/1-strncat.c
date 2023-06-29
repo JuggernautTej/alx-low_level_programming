efbddbffe7eda07283b4b428095072d13814e2b9
JuggernautTej <tiwatej@gmail.com>
2023-06-29 14:49:07 -0700

    updated the strncat function

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -9,14 +9,15 @@
  */
 char *_strncat(char *dest, char *src, int n)
 {
-int i = 0, dest_len = 0;
+int i = 0;
+int dest_len = 0;
 while (dest[i++])
 {
 dest_len++;
 }
 for (i = 0; src[i] && i < n; i++)
 {
-dest[dest_len++] = src[index];
+dest[dest_len++] = src[i];
 }
 return (dest);
 }
