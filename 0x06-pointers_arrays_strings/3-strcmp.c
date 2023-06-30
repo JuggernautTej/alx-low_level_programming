87d30b06d69aef1a594a246dd8b31c9247ae4d7c
JuggernautTej <tiwatej@gmail.com>
2023-06-30 03:36:48 -0700

    added the 3-strcmp program

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,38 @@
+#include"main.h"
+/**
+ *_strcmp - A function that compares strings.
+ *
+ *@s1: destination string.
+ *@s2: Source string.
+ *
+ *Return: integer indicating the result of the comparison.
+ */
+int _strcmp(char *s1, char *s2)
+{
+int i = 0;
+int result1 = s1[i] - s2[i];
+while (s1[i] != '\0' && s2[i] != '\0')
+{
+if (s1[i] < s2[i])
+{
+return (result1);
+}
+else if (s1[i] > s2[i])
+{
+return (result1);
+}
+i++;
+}
+if (s1[i] == '\0' && s2[i] == '\0')
+{
+return (0);
+}
+else if (s1[i] == '\0')
+{
+return (-1);
+}
+else
+{
+return (1);
+}
+}
