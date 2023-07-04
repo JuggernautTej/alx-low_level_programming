d59407f425e0d4ff63a9946ea6df51de7956d71a
JuggernautTej <tiwatej@gmail.com>
2023-07-04 13:46:42 -0700

    added the print_diagsums program

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -12,11 +12,14 @@ void print_diagsums(int *a, int size)
 {
 int main_diagsum = 0;
 int off_diagsum = 0;
-int x;
-for (x = 0; x < size; x++)
+int x, y;
+for (x = 0; x <= (size * size); x = x + size + 1)
 {
-main_diagsum += a[x * size + x];
-off_diagsum += a[(x * size) + (size - 1 - x)];
+main_diagsum = main_diagsum + a[x];
+}
+for (y = size - 1; y <= (size * size) - size; y = y + size - 1)
+{
+off_diagsum = off_diagsum + a[y];
 }
 printf(" %d, %d\n", main_diagsum, off_diagsum);
 }
