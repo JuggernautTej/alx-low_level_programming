6c278ab6614e9face61a48b10f676b001fa532e4
JuggernautTej <tiwatej@gmail.com>
2023-07-05 16:23:49 -0700

    added the print_rev_recursion program

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,20 @@
+#include"main.h"
+/**
+ *_print_rev_recursion - A function that prints the a string.
+ *
+ *@s: pointer to a string.
+ *Return: string @s .
+ */
+void _print_rev_recursion(char *s)
+{
+if (*s == '\0')
+{
+return;
+}
+else
+{
+_print_rev_recursion(s + 1);
+_putchar(*s);
+}
+ _putchar('\0');
+}
