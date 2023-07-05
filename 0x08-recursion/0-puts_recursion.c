dc527b38a92a24bfa1a75146e5b6ed415441db7f
JuggernautTej <tiwatej@gmail.com>
2023-07-05 14:18:40 -0700

    updated the _puts-recursion program7

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -3,17 +3,17 @@
  *_puts_recursion - A function that prints the a string.
  *
  *@s: pointer to a string.
- *Return: void.
+ *Return: string @s .
  */
 void _puts_recursion(char *s)
 {
 if (*s == '\0')
 {
-return (0);
+_putchar('\n');
 }
 else
 {
-return (_puts_recursion(*s + 1]));
-}
 _putchar(*s);
+_puts_recursion(s + 1);
+}
 }
