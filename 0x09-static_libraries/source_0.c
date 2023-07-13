70755ee6c80ffbf11b0953b2d2fc03bf99923923
JuggernautTej <tiwatej@gmail.com>
2023-07-12 17:25:23 -0700

    edited the create_static_lib script again

---
+++ 0x09-static_libraries/source_0.c
@@ -0,0 +1,9 @@
+#include"main.h"
+/**
+ *source_0 - Source 0
+ *Return: Always 0
+ */
+int source_0(void)
+{
+  return (0);
+}
---
+++ 0x09-static_libraries/source_1.c
@@ -0,0 +1,9 @@
+#include"main.h"
+/**
+ *source_1 - Source 1
+ *Return: Always 0
+ */
+int source_1(void)
+{
+  return (0);
+}
