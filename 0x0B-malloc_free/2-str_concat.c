20ec292ba02160e5107005cc26274669890bb3e6
JuggernautTej <tiwatej@gmail.com>
2023-07-12 18:17:22 -0700

    updated the 2-str_concat program

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -14,6 +14,14 @@ char *str_concat(char *s1, char *s2)
 int x = 0;
 int y = 0;
 char *p;
+if (s1 == NULL)
+{
+s1 = "";
+}
+if (s2 == NULL)
+{
+s2 = "";
+}
 p = malloc(sizeof(char) * (strlen(s1) + strlen(s2) + 1));
 if (p == NULL)
 return ("");
