c93ec474c91f5f33efb8ea1cd1694ae0a7e0ca4c
JuggernautTej <tiwatej@gmail.com>
2023-07-15 14:25:04 -0700

    added the string concat program

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -20,5 +20,6 @@ char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
 char *str_concat(char *s1, char *s2);
 void *malloc_checked(unsigned int b);
+char *string_nconcat(char *s1, char *s2, unsigned int n);
 
 #endif
