08ca87d59b43fe23c3f4c0d43409d955dd823100
JuggernautTej <tiwatej@gmail.com>
2023-07-18 11:20:23 -0700

    added the dog header file and new directory with a readme

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - who let the dogs out!
+ * @name: dog name.
+ * @age: dog age
+ * @owner: dog owner
+ */
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+
+#endif
