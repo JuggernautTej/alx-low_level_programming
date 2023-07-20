9478f97fbcdede0eba6bbd05bdbd35fb651b3143
JuggernautTej <tiwatej@gmail.com>
2023-07-20 16:35:08 -0700

    bettyed the sum_them_all program

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -8,17 +8,17 @@
  */
 int sum_them_all(const unsigned int n, ...)
 {
-  va_list tr;
-  int sum = 0;
-  unsigned int x;
-  if (n == 0)
-      return (0);
-  va_start(tr, n);
-  for (x = 0; x < n; x++)
-    {
-      int next = va_arg(tr, int);
-      sum += next;
-    }
-  va_end(tr);
-  return (sum);
+va_list tr;
+int sum = 0;
+unsigned int x;
+if (n == 0)
+return (0);
+va_start(tr, n);
+for (x = 0; x < n; x++)
+{
+int next = va_arg(tr, int);
+sum += next;
+}
+va_end(tr);
+return (sum);
 }
