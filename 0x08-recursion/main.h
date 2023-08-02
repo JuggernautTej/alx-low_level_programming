3904f705472d6b7604400046a4f2eebfd13a169a
JuggernautTej <tiwatej@gmail.com>
2023-08-02 13:54:19 -0700

    added the _print_rev_recursion program

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -2,6 +2,7 @@
 #define MAIN_H
 
 void _puts_recursion(char *s);
+void _print_rev_recursion(char *s);
 int _putchar(char);
 char *_strcat(char *dest, char *src);
 char *_strncpy(char *dest, char *src, int n);
