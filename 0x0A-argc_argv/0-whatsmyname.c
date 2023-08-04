e69c4eca0b4881a3fb8e99f7b0f8a7825b43eeae
JuggernautTej <tiwatej@gmail.com>
2023-08-04 12:07:35 -0700

    added the whatsmyname program

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+/**
+ * main - a function that prints its name followed by a new line
+ * @argc: the argument count as an integer.
+ * @argv: an array of the arguments passed.
+ * Return: 0.
+ */
+int main(int argc, char *argv[])
+{
+int x;
+if (argc >= 1)
+{
+for ( x = 0; x < argc; x++)
+{
+printf("%s\n", argv[0]);
+}
+}
+return (0);
+}
---
+++ 0x0A-argc_argv/READDME.md
@@ -0,0 +1,1 @@
+This directory hosts the tasks for the argc-argv project
