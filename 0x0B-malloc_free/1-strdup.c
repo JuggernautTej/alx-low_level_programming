9557033ee698f5faf522587281ddeb41e6949eb7
JuggernautTej <tiwatej@gmail.com>
2023-08-07 05:30:55 -0700

    added the strdup program

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,46 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include "main.h"
+
+/**
+ * _str_len - function that returns the lenght of a string.
+ * @strg: string.
+ * Return: integer.
+ */
+int _str_len(char *strg)
+{
+if (*strg == '\0')
+return (0);
+else
+return (1 + _str_len(strg + 1));
+}
+/**
+ * _strdup - a function that returns a pointer to a newly allocated space in
+ * memory, which contains a copy of a string given as a parameter.
+ * @str: this is the string.
+ * Return: a pointer to the duplicated string or NULL.
+ */
+char *_strdup(char *str)
+{
+int x;
+char *p;
+int len =  _str_len(str);
+if (str == NULL)
+{
+return (NULL);
+}
+p = malloc(sizeof(char) * (len + 1));
+if (p == NULL)
+{
+return (NULL);
+}
+else
+{
+for (x = 0; x < len; x++)
+{
+p[x] = str[x];
+}
+p[len] = '\0';
+return (p);
+}
+}
