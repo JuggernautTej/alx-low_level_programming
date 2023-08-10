09f211aaa2ee1fffccb923cef4b43e48c6f9aa2f
JuggernautTej <tiwatej@gmail.com>
2023-08-10 09:35:28 -0700

    added the 1-string_nconcat.c

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,63 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include "main.h"
+
+/**
+ * _str_len - function that returns the lenght of a string.
+ * @strg: string.
+ * Return: integer.
+ */
+unsigned int _str_len(char *strg)
+{
+if (*strg == '\0')
+return (0);
+else
+return (1 + _str_len(strg + 1));
+}
+/**
+ * string_nconcat - a function that concatenates two strings.
+ * @s1: string 1.
+ * @s2: string 2.
+ * @n: the first bytes of @s2 to be concatenated.
+ * Return: a pointer to the newly concatenated string or NULL.
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+int x, y, len1, len2;
+char *p;
+if (s1 == NULL)
+{
+s1 = "";
+}
+if (s2 == NULL)
+{
+s2 = "";
+}
+len1 = _str_len(s1);
+if (n >= _str_len(s2))
+{
+len2 = _str_len(s2);
+}
+else
+{
+len2 = n;
+}
+p = malloc(sizeof(char) * (len1 + len2 + 1));
+if (p == NULL)
+{
+return (NULL);
+}
+else
+{
+for (x = 0; x < len1; x++)
+{
+p[x] = s1[x];
+}
+for (y = 0; y < len2; y++)
+{
+p[x + y] = s2[y];
+}
+p[x + y] = '\0';
+return (p);
+}
+}
