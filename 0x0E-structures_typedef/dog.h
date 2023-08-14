e6daaaaa46f958dc7063d52596064b9419e9a5be
JuggernautTej <tiwatej@gmail.com>
2023-08-14 10:22:25 -0700

    updated the dog header file

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,10 +1,16 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * struct dog - a stuct.
+ * @name: name of user.
+ * @age: age of user.
+ * @owner: owner of the dog.
+ */
 struct dog
 {
-  char *name;
-  float age;
-  char *owner;
+char *name;
+float age;
+char *owner;
 };
 #endif
