66c1833fbdbc313564c073c569e3dddfd8f05f7b
JuggernautTej <tiwatej@gmail.com>
2023-08-15 17:57:06 -0700

    added the free_dog program

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include "dog.h"
+
+/**
+ * free_dog - a function that frees dogs.
+ * @d: pointer containing dog details.
+ * Return: void.
+ */
+void free_dog(dog_t *d)
+{
+free(d->name);
+free(d->owner);
+free(d);
+}
