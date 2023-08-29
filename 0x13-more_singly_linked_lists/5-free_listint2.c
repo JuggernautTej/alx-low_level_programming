22be5fc62dc6a9f3564afe7ea128e5b70388cfa3
JuggernautTej <tiwatej@gmail.com>
2023-08-29 13:56:49 -0700

    added the free_listint and free_listint2 programs

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include "lists.h"
+
+/**
+ * free_listint2 - a function that frees a list.
+ * @head: pointer to the start of the list.
+ * Return: void.
+ */
+void free_listint2(listint_t **head)
+{
+listint_t *presentnode, *tmp;
+presentnode = *head;
+while (presentnode != NULL)
+{
+tmp = presentnode->next;
+free(presentnode);
+presentnode = tmp;
+}
+*head = NULL;
+}
