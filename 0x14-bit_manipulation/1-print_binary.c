b4ff39278482ef7b3dd447840a086a3f2b39b590
JuggernautTej <tiwatej@gmail.com>
2023-08-31 18:27:42 -0700

    added the print_binary program

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include "main.h"
+
+/**
+ * print_binary - a function that prints the binary of a number.
+ * @n: the number.
+ * Return: void.
+ */
+void print_binary(unsigned long int n)
+{
+if (n == 0)
+{
+_putchar('0');
+return;
+}
+print_binary(n >> 1);
+_putchar((n & 1) + '0');
+}
