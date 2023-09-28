36b1452b2b8175d99c41367894d08a615c6415f0
JuggernautTej <tiwatej@gmail.com>
2023-09-28 16:29:53 -0700

    updated the binary_to_uint program

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -9,10 +9,10 @@
 unsigned int binary_to_uint(const char *b)
 {
 unsigned int unit = 0;
-if (*b > '1' || *b < '0' || b == NULL)
-return (0);
 while (*b != '\0')
 {
+if (*b != '0' && *b != '1')
+return (0);
 unit <<= 1;
 unit |= (*b == '1') ? 1 : 0;
 b++;
