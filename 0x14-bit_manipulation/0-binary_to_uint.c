e8ea218d03ead3b5c450c2ff55aa36b3e211336f
JuggernautTej <tiwatej@gmail.com>
2023-11-23 05:04:21 -0800

    updated the binary_to_uint

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -2,7 +2,7 @@
 #include "main.h"
 
 /**
- * binary_to_unit - a function that converts a binary number to an unsigned int
+ * binary_to_uint - a function that converts a binary number to an unsigned int
  * @b: a pointer to a tring containing 0 and 1.
  * Return: the converted number or 0.
  */
