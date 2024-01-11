550a24f3dcf156569de1c8914bbfa361b53aa190
JuggernautTej <tiwatej@gmail.com>
2024-01-11 12:15:27 -0800

    edited the 0 task file

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -11,7 +11,7 @@ size_t print_dlistint(const dlistint_t *h)
 const dlistint_t *current;
 size_t tally = 0;
 if (h == NULL)
-return (EXIT_FAILURE);
+return (0);
 for (current = h; current != NULL;  current = current->next)
 {
 printf("%d\n", current->n);
